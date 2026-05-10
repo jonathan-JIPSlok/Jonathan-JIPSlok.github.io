a6825dec52b772c31122a0834d5d490cbaeb4e10
jonathan-jipslok <jonathanpoli17@gmail.com>
2026-05-10 11:25:37 -0300

    feat: arrumando de imagem a video e adicionei o JSON v1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/*******************************************************************************************
+ * Obejtivo: adicionar projetos direto no index a partir de dados de um JSON
+ * Autor: Jonathan Yuri Poli Siqueira
+ * Data: 10/05/2026
+ */
+
